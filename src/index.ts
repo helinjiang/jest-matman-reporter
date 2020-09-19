8cb31dc5d79d9c2ff1a78736e1b762471d750ea8
linjianghe <linjianghe@tencent.com>
2020-09-19 20:20:22 +0800

    feat: 自定义打印

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,117 @@
-const a = 'hell';
+import { Config } from '@jest/types';
+import { Context, Test, ReporterOnStartOptions } from '@jest/reporters';
+import { AggregatedResult, TestResult } from '@jest/test-result';
 
-export { a };
+import { IJestMatmanReporterOptions } from './types';
+
+// https://brunoscheufler.com/blog/2020-02-14-supercharging-jest-with-custom-reporters
+// https://github.com/dkelosky/jest-stare/blob/master/src/reporter/Reporter.ts
+
+// import { Reporter } from '@jest/reporters';
+// export default class JestMatmanReporter implements Pick<Reporter, 'onRunComplete'> {
+export default class JestMatmanReporter {
+  public mGlobalConfig: Config.InitialOptions;
+
+  /**
+   * jest-matman-reporter configuration
+   * @private
+   * @type {IJestMatmanReporterOptions}
+   * @memberof Reporter
+   */
+  private mOptions: IJestMatmanReporterOptions;
+
+  /**
+   * Creates an instance of Reporter.
+   * @param {GlobalConfig} mGlobalConfig - jest global config
+   * @param {*} mOptions - jest options in effect
+   * @memberof Reporter
+   */
+  constructor(mGlobalConfig: Config.InitialOptions, mOptions: IJestMatmanReporterOptions) {
+    this.mGlobalConfig = mGlobalConfig;
+    this.mOptions = mOptions;
+  }
+
+  /**
+   * Call for tests starting
+   * @param {AggregatedResult} results - jest results
+   * @param {ReporterOnStartOptions} options - jest invoked options
+   * @memberof Reporter
+   */
+  public onRunStart(results: AggregatedResult, options?: ReporterOnStartOptions) {
+    // do nothing
+  }
+
+  /**
+   * Called for single test
+   * @param {Test} test - jest Test object
+   * @memberof Reporter
+   */
+  public onTestStart(test: Test) {
+    // do nothing
+  }
+
+  /**
+   * Called on a test completion
+   * @param {Test} test - jest Test object
+   * @param {TestResult} testResult - jest results
+   * @param {AggregatedResult} aggregatedResult - jest summarized results
+   * @memberof Reporter
+   */
+  public onTestResult(test: Test, testResult: TestResult, results: AggregatedResult) {
+    // do nothing
+  }
+
+  /**
+   * Called when all is complete?
+   * @param {Set<Context>} contexts - jest context
+   * @param {IResultsProcessorInput} results - jest summarized results
+   * @memberof Reporter
+   */
+  public onRunComplete(contexts: Set<Context>, results: AggregatedResult) {
+    if (this.mOptions?.outputPath) {
+      console.log('=this.mOptions?.outputPath=', this.mOptions?.outputPath);
+    }
+
+    // 这里的取值参考了 @jest/reporters 里面 utils.js 中的写法
+    const suitesFailed = results.numFailedTestSuites;
+    const suitesPassed = results.numPassedTestSuites;
+    const suitesPending = results.numPendingTestSuites;
+    const suitesTotal = results.numTotalTestSuites;
+
+    const testsFailed = results.numFailedTests;
+    const testsPassed = results.numPassedTests;
+    const testsPending = results.numPendingTests;
+    const testsTotal = results.numTotalTests;
+
+    const result = {
+      testSuites: {
+        // 未通过的数量
+        failed: suitesFailed,
+
+        // 跳过不执行的数量
+        skipped: suitesPending,
+
+        // 通过的数量
+        passed: suitesPassed,
+
+        // 总数
+        total: suitesTotal,
+      },
+      tests: {
+        // 未通过的数量
+        failed: testsFailed,
+
+        // 跳过不执行的数量
+        skipped: testsPending,
+
+        // 通过的数量
+        passed: testsPassed,
+
+        // 总数
+        total: testsTotal,
+      },
+    };
+
+    console.log('result: ', result);
+  }
+}
